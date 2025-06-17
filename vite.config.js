6b852df08d0c5c6297592c2ae8abcf1aa2215adc
Okan Özdemir <ookanozdemir@gmail.com>
2025-06-17 18:50:16 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/goit-react-hw-07/',
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
