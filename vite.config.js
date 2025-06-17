acdcb6ae95e651f80c20f9f077fd83834cb7d5fc
Okan Özdemir <ookanozdemir@gmail.com>
2025-06-17 18:50:33 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,10 +7,10 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
-      target: 'es2020'
-    }
+      target: 'es2020',
+    },
   },
   esbuild: {
-    target: 'es2020'
-  }
-})
+    target: 'es2020',
+  },
+});
